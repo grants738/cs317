e0b447d51d2e9e1274c18b4097dc9f418e500c3e
Grant Savage <grantsavage1@me.com>
2017-11-28 22:45:44 -0700

    Updated app name

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Cart',
+    'name' => 'Siewerts and Sundries',
 
     /*
     |--------------------------------------------------------------------------
