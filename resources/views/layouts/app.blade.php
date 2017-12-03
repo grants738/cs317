d4045d2c654b73fe1365fe49efcee0bc65565bbd
Grant Savage <grantsavage1@me.com>
2017-12-03 00:39:24 -0700

    Added dynamic project name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
         				<span class="icon-bar"></span>
         				<span class="icon-bar"></span>
       				</button>
-		      		<a class="navbar-brand" href="{{route('home')}}">Cart</a>
+		      		<a class="navbar-brand" href="{{route('home')}}">{{config('app.name')}}</a>
 		    	</div>
 
 			    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
